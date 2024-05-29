77e5f931a8d0c89054632bf11ea15b4104f2b5a0
Noorhesham <noordragon2004@gmail.com>
2024-05-29 11:52:09 +0300

    meow

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,7 +8,7 @@ const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Noor Hesham Portfolio",
-  icons:{icon:'/icon.png'},
+  icons:{icon:'/logo.png'},
   description:
     "MERN Stack Developer specializing in React, Next.js, TypeScript,Node .js,Mongo DB and Tailwind CSS. Proven track record of crafting high-performance web applications with sleek, user-friendly interfaces. Adaptable and detail-oriented, committed to delivering top-notch solutions. Passionate about staying ahead in technology and contributing to dynamic teams.",
 };
